26a5532b290f3e23b1b33259a6a0ef45a5d9ea2b
Arthur Shaw <jashaw2002@gmail.com>
2024-07-09 06:49:24 -0400

    Set up boilerplate for module to manager module

--- R/mod_3_complete_1_setup_2_clusters_3_manager_id.R
+++ R/mod_3_complete_1_setup_2_clusters_3_manager_id.R
@@ -11,6 +11,23 @@ mod_3_complete_1_setup_2_clusters_3_manager_id_ui <- function(id){
   ns <- NS(id)
   shiny::tagList(
  
+    bslib::accordion(
+      id = ns("manager_id_steps"),
+      open = FALSE,
+      bslib::accordion_panel(
+        title = "1. Select variables",
+        value = "select_vars_panel",
+      ),
+      bslib::accordion_panel(
+        title = "2. Order variables",
+        value = "order_vars_panel",
+      ),
+      bslib::accordion_panel(
+        title = "3. Compose description",
+        value = "compose_desc_panel",
+      )
+    )
+
   )
 }
     
@@ -20,6 +37,73 @@ mod_3_complete_1_setup_2_clusters_3_manager_id_ui <- function(id){
 mod_3_complete_1_setup_2_clusters_3_manager_id_server <- function(id, parent, r6){
   moduleServer( id, function(input, output, session){
     ns <- session$ns
+
+    # ==========================================================================
+    # load child module definitions
+    # ==========================================================================
+
+
+    # ==========================================================================
+    # move from one accordion panel to the next
+    # ==========================================================================
+
+    # from computer ID to select
+    gargoyle::on("save_computer_identify_clusters", {
+
+      # open order
+      bslib::accordion_panel_open(
+        id = "manager_id_steps",
+        value = "select_vars_panel"
+      )
+
+    })
+
+    # from select to order
+    gargoyle::on("save_manager_select_clusters", {
+
+      # close select
+      bslib::accordion_panel_close(
+        id = "manager_id_steps",
+        value = "select_vars_panel"
+      )
+
+      # open order
+      bslib::accordion_panel_open(
+        id = "manager_id_steps",
+        value = "order_vars_panel"
+      )
+
+    })
+
+    # from order to compose
+    gargoyle::on("save_manager_order_clusters", {
+
+      # close order
+      bslib::accordion_panel_close(
+        id = "manager_id_steps",
+        value = "order_vars_panel"
+      )
+
+      # open compose
+      bslib::accordion_panel_open(
+        id = "manager_id_steps",
+        value = "compose_desc_panel"
+      )
+
+    })
+
+    # from compose to team workload
+    # note: opening team workload is managed in complete report setup module
+    gargoyle::on("save_manager_compose_clusters", {
+
+      # close compose
+      bslib::accordion_panel_close(
+        id = "manager_id_steps",
+        value = "compose_desc_panel"
+      )
+
+    })
+
 
   })
 }
