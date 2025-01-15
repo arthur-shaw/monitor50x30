fdf37ab7057536059629f0238a61f594978cf923
Arthur Shaw <jashaw2002@gmail.com>
2025-01-15 10:02:13 -0500

    Add fun to render reports
    Borrowed from monitorMTF

--- R/fct_helpers.R
+++ R/fct_helpers.R
@@ -128,4 +128,100 @@ create_team_choices <- function(
 
 }
 
+#' Perform Quarto report rendering workflow
+#'
+#' @description
+#' Workflow:
+#' - Determine which report template to use
+#' - Copy that template, ana associated resources, from the package to the app
+#' - Render the report where the template and resources have been copied
+#' - Move the rendered report to a user-facing folder
+#'
+#' @param report_type Character. One of: completeness, quality
+#' @param proj_dir Character. Path to the root of the app directory
+#' @param params List. Parameters for Quarto report.
+#'
+#' @importFrom glue glue
+#' @importFrom dplyr case_when
+#' @importFrom fs path path_package file_copy file_move
+#' @importFrom quarto quarto_render
+#'
+#' @return Side-effect of producing a rendered report in a certain directory
+render_report <- function(
+  report_type,
+  proj_dir,
+  params
+) {
+
+  # ============================================================================
+  # set paths as a function of project path and report type
+  # ============================================================================
+
+  # ----------------------------------------------------------------------------
+  # app paths
+  # ----------------------------------------------------------------------------
+
+  report_name <- glue::glue("report_{report_type}.qmd")
+
+  report_dir <- dplyr::case_when(
+    report_type == "completeness" ~ "01_completeness",
+    report_type == "quality" ~ "02_quality",
+    TRUE ~ ""
+  )
+
+  # top-level report-specific directory
+  report_app_dir <- fs::path(
+    proj_dir, "02_reports", report_dir
+  )
+
+  # where template should be copied
+  template_app_path <- fs::path(
+    report_app_dir, "resources", report_name
+  )
+
+  # ----------------------------------------------------------------------------
+  # package paths
+  # ----------------------------------------------------------------------------
+
+  pkg_path <- fs::path_package(
+    package = "monitor50x30",
+    "quarto", "templates"
+  )
+
+  template_pkg_path <- fs::path(
+    pkg_path, glue::glue("{report_type}"), report_name
+  )
+
+  # ============================================================================
+  # copy resources from package to app
+  # ============================================================================
+
+  fs::file_copy(
+    path = template_pkg_path,
+    new_path = template_app_path,
+    overwrite = TRUE
+  )
+
+  # TODO: add files/revise approach as needed
+
+  # ============================================================================
+  # render report in situ
+  # ============================================================================
+
+  quarto::quarto_render(
+    input = template_app_path,
+    execute_params = params
+  )
+
+  # ============================================================================
+  # move report to user-facing report dir
+  # ============================================================================
+
+  fs::file_move(
+    path = fs::path(
+      report_app_dir, "resources", glue::glue("report_{report_type}.html")
+    ),
+    new_path = fs::path(report_app_dir, glue::glue("report_{report_type}.html"))
+  )
+
 }
