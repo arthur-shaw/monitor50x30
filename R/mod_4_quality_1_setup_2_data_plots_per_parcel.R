e63abeb0c5db7ca5632c78a4bc04a908047b145c
Arthur Shaw <jashaw2002@gmail.com>
2025-09-21 18:40:52 -0400

    fix(data_plots_per_parcel): Handle init when never saved values

--- R/mod_4_quality_1_setup_2_data_plots_per_parcel.R
+++ R/mod_4_quality_1_setup_2_data_plots_per_parcel.R
@@ -79,6 +79,32 @@ mod_4_quality_1_setup_2_data_plots_per_parcel_server <- function(
     })
 
     # --------------------------------------------------------------------------
+    # load null values since values not previously saved
+    # --------------------------------------------------------------------------
+
+    if (is.null(r6$plots_per_parcel_provided)) {
+
+      # update UI to reflect data choices
+      # but do not trigger reactive
+      shiny::freezeReactiveValue(input, "data")
+      shiny::updateSelectInput(
+        inputId = "data",
+        choices = r6$data_choices,
+        selected = NULL
+      )
+
+      # (re)set to `NULL` variable and value selections
+      # but do not trigger reactive
+      shiny::updateSelectInput(
+        inputId = "parcel_id_var",
+        choices = NULL,
+        selected = NULL
+      )
+
+    }
+
+
+    # --------------------------------------------------------------------------
     # load past selections from R6
     # --------------------------------------------------------------------------
 
