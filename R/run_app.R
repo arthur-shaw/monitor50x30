cad35e7e51a5534c6930ba78f2422aa5571946bc
Arthur Shaw <jashaw2002@gmail.com>
2026-06-29 18:48:31 -0400

    feat(depdencies): check Quarto installed; error otherwise
    Closes #15

--- R/run_app.R
+++ R/run_app.R
@@ -7,6 +7,8 @@
 #' @export
 #' @importFrom shiny shinyApp
 #' @importFrom golem with_golem_options
+#' @importFrom quarto quarto_path
+#' @importFrom cli cli_abort cli_info
 run_app <- function(
   onStart = NULL,
   options = list(),
@@ -14,6 +16,28 @@ run_app <- function(
   uiPattern = "/",
   ...
 ) {
+
+  # check for system dependencies
+  # Quarto
+  if (is.null(quarto::quarto_path())) {
+    cli::cli_abort(
+      message = c(
+        "x" = "Quarto cannot be found. Please install it.",
+        "i" = "To create reports, this application needs Quarto installed.",
+        "!" = paste(
+          "To install it, follow the instructions here:",
+          "https://quarto.org/docs/get-started/"
+        )
+      )
+    )
+  } else {
+    cli::cli_inform(
+      message = c(
+        "v" = "Quarto found."
+      )
+    )
+  }
+
   with_golem_options(
     app = shinyApp(
       ui = app_ui,
